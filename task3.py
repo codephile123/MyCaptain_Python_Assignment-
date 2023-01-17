28b652fafa258e0198b68467935e99ab9937c23e
codephile123 <122461147+codephile123@users.noreply.github.com>
2023-01-17 20:59:11 +0530

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,13 @@
+r = float(input("Input the radius of the circle : "))
+
+area = 3.14159265359 * r * r
+
+print(area)
+
+
+
+file_name = input("Input the FileName : ")
+
+if 'py' in file_name:
+
+print('The extension of the file is: \'python\'')
